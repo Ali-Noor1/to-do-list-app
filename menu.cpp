f3e8336e2849125e9da4d25006cc02a868febb84
muhammad-atif-khan-1 <matifkhan1068@gmail.com>
2025-10-19 19:17:27 +0500

    Menue Module is added

---
+++ menu.cpp
@@ -0,0 +1,10 @@
+// Function to display the menu
+void showMenu() {
+    cout << "\n========== MENU ==========\n";
+    cout << "1. Add Task\n";
+    cout << "2. View Tasks\n";
+    cout << "3. Mark Task as Completed\n";
+    cout << "4. Delete Task\n";
+    cout << "5. Exit\n";
+    cout << "==========================\n";
+}
